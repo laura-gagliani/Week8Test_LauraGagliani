424c678fc68d5506438f2a6652459b35472d6605
Laura Gagliani <gaglianilaura@gmail.com>
2021-11-26 12:24:22 +0100

    scheletro ADO

---
+++ Rubrica/Rubrica.RepositoryADO/ContattiADO.cs
@@ -0,0 +1,33 @@
+﻿using Rubrica.Core.Entities;
+using Rubrica.Core.RepoInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubrica.RepositoryADO
+{
+    class ContattiADO : IRepositoryContatti
+    {
+        public bool Add(Contatto item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Contatto c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contatto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contatto GetById(int idContatto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rubrica/Rubrica.RepositoryADO/IndirizziADO.cs
@@ -0,0 +1,18 @@
+﻿using Rubrica.Core.Entities;
+using Rubrica.Core.RepoInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubrica.RepositoryADO
+{
+    class IndirizziADO : IRepositoryIndirizzi
+    {
+        public bool Add(Indirizzo item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
